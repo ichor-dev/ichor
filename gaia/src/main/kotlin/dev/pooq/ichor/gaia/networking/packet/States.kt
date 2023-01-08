fdd32bd938bf1052ec8c38ba32c9b9c5b2604a0e
Paul Kindler <paul.kindler@staffbase.com>
2023-01-08 23:08:26 +0100

    add State

---
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/networking/packet/States.kt
@@ -0,0 +1,12 @@
+package dev.pooq.ichor.gaia.networking.packet
+
+enum class State(
+  val stateName: String
+){
+
+  HANDSHAKING("Handshaking"),
+  STATUS("Status"),
+  LOGIN("Login"),
+  PLAY("Play")
+
+}
