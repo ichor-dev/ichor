a28058680dc455488808c268284429091ef73bed
Paul Kindler <paul.kindler@staffbase.com>
2023-02-06 17:25:30 +0100

    add entity, creature and player

---
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/entity/Entity.kt
@@ -0,0 +1,8 @@
+package dev.pooq.ichor.gaia.entity
+
+interface Entity {
+
+  interface Creature : Entity{
+
+  }
+}
---
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/entity/player/Player.kt
@@ -0,0 +1,12 @@
+package dev.pooq.ichor.gaia.entity.player
+
+import dev.pooq.ichor.gaia.entity.Entity
+import dev.pooq.ichor.gaia.networking.handle.PacketHandle
+
+class Player(
+  val handle: PacketHandle
+) : Entity.Creature {
+
+
+
+}
