3fb10c4ac2b06ccb1040ffc840a73d084681eee5
Paul Kindler <paul.kindler@staffbase.com>
2023-02-15 22:46:45 +0100

    :heavy_plus_sign: add fields to player

--- gaia/src/main/kotlin/dev/pooq/ichor/gaia/entity/player/Player.kt
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/entity/player/Player.kt
@@ -2,7 +2,10 @@ package dev.pooq.ichor.gaia.entity.player
 
 import dev.pooq.ichor.gaia.entity.Entity
 import dev.pooq.ichor.gaia.networking.packet.PacketHandle
+import java.util.*
 
 class Player(
+  val uuid: UUID,
+  val name: String,
   val handle: PacketHandle
 ) : Entity.Creature
