53d410c27538c1cf0607dee70bf76cf050cc5d62
Anton <anton.wiblishauser@gmx.de>
2023-02-16 21:02:23 +0100

    remake some bytebuffer extensions

--- gaia/src/main/kotlin/dev/pooq/ichor/gaia/extensions/ByteBufferExtensions.kt
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/extensions/ByteBufferExtensions.kt
@@ -1,8 +1,6 @@
 package dev.pooq.ichor.gaia.extensions
 
 import java.nio.ByteBuffer
-import kotlin.experimental.and
-import kotlin.experimental.or
 
 private const val SEGMENT_BITS = 0x7F
 private const val CONTINUE_BIT = 0x80
@@ -11,41 +9,27 @@ inline fun buffer(capacity: Int, applier: ByteBuffer.() -> Unit = {}): ByteBuffe
   ByteBuffer.allocate(capacity).apply(applier).flip()
 
 fun ByteBuffer.varInt(): Int {
-  var value = 0
-  var position = 0
-  var currentByte: Byte
-
-  while (true) {
-    currentByte = get()
-    value = value or ((currentByte.toInt() and SEGMENT_BITS) shl position)
-
-    if (currentByte.toInt() and CONTINUE_BIT == 0) break
-
-    position += 7
-
-    if (position >= 32) throw RuntimeException("VarInt is too big")
-  }
-
-  return value
+  var result = 0
+  var shift = 0
+  var b: Byte
+  do {
+    b = this.get()
+    result = result or ((b.toInt() and SEGMENT_BITS) shl shift)
+    shift += 7
+  } while (b < 0)
+  return result
 }
 
 fun ByteBuffer.varLong(): Long {
-  var value: Long = 0
-  var position = 0
-  var currentByte: Byte
-
-  while (true) {
-    currentByte = get()
-    value = value or (((currentByte.toInt() and SEGMENT_BITS) shl position).toLong())
-
-    if (currentByte.toInt() and CONTINUE_BIT == 0) break
-
-    position += 7
-
-    if (position >= 64) throw RuntimeException("VarLong is too big")
-  }
-
-  return value
+  var result: Long = 0
+  var shift = 0
+  var b: Byte
+  do {
+    b = this.get()
+    result = result or ((b.toLong() and SEGMENT_BITS.toLong()) shl shift)
+    shift += 7
+  } while (b < 0)
+  return result
 }
 
 fun ByteBuffer.boolean(): Boolean {
@@ -59,66 +43,41 @@ fun ByteBuffer.boolean(boolean: Boolean) {
 fun ByteBuffer.string(): String {
   val length = varInt()
   val bytes = ByteArray(length)
-
   get(bytes)
-
   return String(bytes, Charsets.UTF_8)
 }
-
 
 fun ByteBuffer.short(): Short {
   return (get().toInt() and 0xFF shl 8 or (get().toInt() and 0xFF)).toShort()
 }
 
 fun ByteBuffer.varLong(long: Long) {
-  var value = long
-  while (true) {
-    if (value and 0x7FL.inv() == 0L) {
-      put(value.toByte())
-      return
-    } else {
-      put((value and 0x7F or 0x80).toByte())
-      value = value ushr 7
-    }
+  var remainingValue = long
+  while (remainingValue and SEGMENT_BITS.toLong().inv() != 0L) {
+    this.put((remainingValue and SEGMENT_BITS.toLong()).or(CONTINUE_BIT.toLong()).toByte())
+    remainingValue = remainingValue ushr 7
   }
+  this.put(remainingValue.toByte())
 }
 
 fun ByteBuffer.varInt(int: Int) {
-  var value = int
-  while (true) {
-    if (value.toLong() and 0x7FL.inv() == 0L) {
-      put(value.toByte())
-      return
-    } else {
-      put((value and 0x7F or 0x80).toByte())
-      value = value ushr 7
-    }
+  var remainingValue = int
+  while (remainingValue and 0xFFFFFF80.toInt() != 0) {
+    this.put(((remainingValue and SEGMENT_BITS) or CONTINUE_BIT).toByte())
+    remainingValue = remainingValue ushr 7
   }
+  this.put(remainingValue.toByte())
 }
 
 fun ByteBuffer.string(string: String) {
-  val maxLength = Short.MAX_VALUE
-
-  if (string.length > maxLength) throw IllegalArgumentException("String is too long.")
-
-  val bytes = string.toByteArray(Charsets.UTF_8)
-
-  if (bytes.size > (maxLength * 4) + 3) throw throw IllegalArgumentException("String bytearray is too long.")
-
-  put(bytes)
+  val encoded = string.toByteArray(Charsets.UTF_8)
+  varInt(encoded.size)
+  put(encoded)
 }
 
 fun ByteBuffer.short(short: Short) {
-  var value = short
-  while (true) {
-    if (value.toLong() and 0x7FL.inv() == 0L) {
-      put(value.toByte())
-      return
-    } else {
-      put((value and 0x7F or 0x80).toByte())
-      value = (value.toInt() ushr 7).toShort()
-    }
-  }
+  this.put((short.toInt() ushr 8 and 0xFF).toByte())
+  this.put((short.toInt() and 0xFF).toByte())
 }
 
 fun ByteBuffer.byteArray(length: Int) = ByteArray(length).also(this::get)
