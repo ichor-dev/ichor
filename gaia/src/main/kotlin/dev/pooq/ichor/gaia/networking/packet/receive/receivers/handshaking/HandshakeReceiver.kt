69d74d4ac3d4a187e8f597624cbedc95251ffbad
Anton <anton.wiblishauser@gmx.de>
2023-02-16 21:00:39 +0100

    change debug message in HandshakeReceiver

--- gaia/src/main/kotlin/dev/pooq/ichor/gaia/networking/packet/receive/receivers/handshaking/HandshakeReceiver.kt
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/networking/packet/receive/receivers/handshaking/HandshakeReceiver.kt
@@ -1,6 +1,8 @@
 package dev.pooq.ichor.gaia.networking.packet.receive.receivers.handshaking
 
+import com.github.ajalt.mordant.rendering.TextColors
 import dev.pooq.ichor.gaia.extensions.debug.debug
+import dev.pooq.ichor.gaia.extensions.terminal
 import dev.pooq.ichor.gaia.networking.packet.PacketHandle
 import dev.pooq.ichor.gaia.networking.packet.client.handshaking.Handshake
 import dev.pooq.ichor.gaia.networking.packet.receive.PacketReceiver
@@ -9,7 +11,7 @@ import dev.pooq.ichor.gaia.server.Server
 class HandshakeReceiver : PacketReceiver<Handshake> {
 
   override suspend fun onReceive(packet: Handshake, packetHandle: PacketHandle, server: Server) {
+    terminal.debug(TextColors.brightBlue("Handshake Packet with next state: ${packet.nextState}, protocol version: ${packet.protocolVersion}"))
     packetHandle.state = packet.nextState
-    server.terminal.debug("Changed state of ${packetHandle.socket.remoteAddress} to ${packetHandle.state}")
   }
 }
