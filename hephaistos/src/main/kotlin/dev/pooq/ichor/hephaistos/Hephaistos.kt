c882265679b28d15fe97d525c7f18c1ae4d4d4e0
Anton <anton.wiblishauser@gmx.de>
2023-04-10 20:07:50 +0200

    rename value to size in packet read

--- hephaistos/src/main/kotlin/dev/pooq/ichor/hephaistos/Hephaistos.kt
+++ hephaistos/src/main/kotlin/dev/pooq/ichor/hephaistos/Hephaistos.kt
@@ -43,16 +43,16 @@ object Hephaistos : Server() {
       launch {
         try {
           while (!connection.input.isClosedForRead) {
-            var value = 0
+            var size = 0
             var i = 0
             var b: Byte
             do {
               b = connection.input.readByte()
-              value = value or ((b.toInt() and 0x7F) shl (i * 7))
+              size = size or ((b.toInt() and 0x7F) shl (i * 7))
               i++
             } while ((b.toInt() and 0x80) != 0 && i < 5)
 
-            val buffer = ByteBuffer.allocate(value)
+            val buffer = ByteBuffer.allocate(size)
             connection.input.readAvailable(buffer)
             buffer.flip()
 
