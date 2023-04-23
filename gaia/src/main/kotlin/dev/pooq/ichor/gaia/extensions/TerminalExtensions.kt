41cb1d8690fbaef4df91420eefe1d7b505645809
Paul Kindler <paul.kindler@staffbase.com>
2023-04-23 21:33:26 +0200

    bump gradle, update terminal + debug extensions

--- gaia/src/main/kotlin/dev/pooq/ichor/gaia/extensions/TerminalExtensions.kt
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/extensions/TerminalExtensions.kt
@@ -1,25 +1,33 @@
 package dev.pooq.ichor.gaia.extensions
 
 import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.rendering.TextStyle
 import com.github.ajalt.mordant.rendering.TextStyles.bold
 import com.github.ajalt.mordant.rendering.Theme
 import com.github.ajalt.mordant.terminal.Terminal
 
-fun Terminal.error(message: Any? = null, error: Throwable) {
-  val errorStyle = (brightRed + bold)
 
+val Terminal.debugStyle: TextStyle
+  get() = (brightBlue + bold)
+
+val Terminal.errorStyle: TextStyle
+  get() = (brightRed + bold)
+
+val Terminal.warnStyle: TextStyle
+  get() = (brightYellow + bold)
+
+val Terminal.logStyle: TextStyle
+  get() = (brightGreen + bold)
+
+fun Terminal.error(message: Any? = null, error: Throwable) {
   this.println(errorStyle("[ERROR]") + white(" > ") + red("${message.let { "${it ?: ""}: " }}${error.stackTraceToString()}"))
 }
 
 fun Terminal.warn(message: Any?) {
-  val logStyle = (brightYellow + bold)
-
-  this.println(logStyle("[WARN]") + white("  > ") + yellow(message.toString()))
+  this.println(warnStyle("[WARN]") + white("  > ") + yellow(message.toString()))
 }
 
 fun Terminal.log(message: Any) {
-  val logStyle = (brightGreen + bold)
-
   this.println(logStyle("[INF0]") + white("  > ") + brightWhite(message.toString()))
 }
 
--- gaia/src/main/kotlin/dev/pooq/ichor/gaia/extensions/debug/DebugExtensions.kt
+++ gaia/src/main/kotlin/dev/pooq/ichor/gaia/extensions/debug/DebugExtensions.kt
@@ -3,12 +3,12 @@ package dev.pooq.ichor.gaia.extensions.debug
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.bold
 import com.github.ajalt.mordant.terminal.Terminal
+import dev.pooq.ichor.gaia.extensions.debugStyle
 import dev.pooq.ichor.gaia.extensions.env
 
 var debug: Boolean = env("debug").toBoolean()
 
 fun Terminal.debug(message: Any, force: Boolean = false) {
   if (!debug && !force) return
-  val debugStyle = (brightBlue + bold)
   this.println(debugStyle("[DEBUG]") + white(" > ") + blue(message.toString()))
 }
