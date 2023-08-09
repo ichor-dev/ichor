9c1c9f836fe040bada364e27173906ea911b6c7a
Anton <anton.wiblishauser@gmx.de>
2023-08-09 19:01:09 +0200

    cleanup Hephaistos

--- hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/Hephaistos.kt
+++ hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/Hephaistos.kt
@@ -1,7 +1,7 @@
 package fyi.pauli.ichor.hephaistos
 
-import fyi.pauli.ichor.gaia.networking.packet.incoming.IncomingPacketHandler
 import fyi.pauli.ichor.gaia.server.Server
+import fyi.pauli.ichor.hephaistos.extensions.handleIncoming
 import io.github.oshai.kotlinlogging.KLogger
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
@@ -13,7 +13,6 @@ import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.core.context.startKoin
 import org.koin.dsl.lazyModule
 import java.net.SocketException
-import java.nio.ByteBuffer
 
 @OptIn(KoinExperimentalAPI::class)
 suspend fun main() {
@@ -32,24 +31,18 @@ suspend fun main() {
 class Hephaistos : Server("Hephaistos") {
 
 	override suspend fun startup() {
-
 		logger.info {
 			"Starting server..."
 		}
 
-		val server = config.server
+		val serverConfig = config.server
 
 		val manager = SelectorManager(Dispatchers.IO)
-		val serverSocket =
-			aSocket(manager).tcp().bind(
-				InetSocketAddress(
-					server.host, server.port.toInt()
-				)
+		val serverSocket = aSocket(manager).tcp().bind(
+			InetSocketAddress(
+				serverConfig.host, serverConfig.port.toInt()
 			)
-
-		players.forEach {
-			it.handle.connection.handle()
-		}
+		)
 
 		logger.info {
 			"Server started successfully!"
@@ -64,27 +57,11 @@ class Hephaistos : Server("Hephaistos") {
 
 			launch {
 				try {
-					while (!connection.input.isClosedForRead) {
-						var size = 0
-						var i = 0
-						var b: Byte
-						do {
-							b = connection.input.readByte()
-							size = size or ((b.toInt() and 0x7F) shl (i * 7))
-							i++
-						} while ((b.toInt() and 0x80) != 0 && i < 5)
-
-						val buffer = ByteBuffer.allocate(size)
-						connection.input.readAvailable(buffer)
-						buffer.flip()
-
-						IncomingPacketHandler.deserializeAndHandle(buffer, handle, this@Hephaistos)
-					}
+					connection.handleIncoming(handle, this@Hephaistos)
 				} catch (e: Throwable) {
-					if (e !is ClosedReceiveChannelException && e !is SocketException)
-						logger.error(e) {
-							"Error in channel"
-						}
+					if (e !is ClosedReceiveChannelException && e !is SocketException) logger.error(e) {
+						"Error in channel"
+					}
 				} finally {
 					connection.input.cancel()
 					connection.output.close()
