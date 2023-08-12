21e68ca3bec0e5631b2e49e280f486c2fcd4373d
Anton <anton.wiblishauser@gmx.de>
2023-08-12 22:38:39 +0200

    add payloads to prevent manual bytearray sending

---
+++ gaia/src/main/kotlin/fyi/pauli/ichor/gaia/models/payload/BrandPayload.kt
@@ -0,0 +1,13 @@
+package fyi.pauli.ichor.gaia.models.payload
+
+import fyi.pauli.ichor.gaia.models.Identifier
+import java.nio.ByteBuffer
+
+data class BrandPayload(val brand: String): Payload {
+	override val identifier: Identifier
+		get() = Identifier("minecraft", "brand")
+
+	override fun write(buffer: ByteBuffer) {
+		TODO("Not yet implemented")
+	}
+}
