872a5b534f94fe1a0965ef510ec50f0f0a4cb68f
Paul Kindler <paul.kindler@staffbase.com>
2023-08-12 21:27:50 +0200

    update handleIncoming function

--- hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/Hephaistos.kt
+++ hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/Hephaistos.kt
@@ -57,7 +57,7 @@ class Hephaistos : Server("Hephaistos") {
 
 			launch {
 				try {
-					connection.handleIncoming(handle, this@Hephaistos)
+					handle.handleIncoming(this@Hephaistos)
 				} catch (e: Throwable) {
 					if (e !is ClosedReceiveChannelException && e !is SocketException) logger.error(e) {
 						"Error in channel"
--- hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/extensions/ConnectionExtensions.kt
+++ hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/extensions/ConnectionExtensions.kt
@@ -3,24 +3,23 @@ package fyi.pauli.ichor.hephaistos.extensions
 import fyi.pauli.ichor.gaia.networking.packet.PacketHandle
 import fyi.pauli.ichor.gaia.networking.packet.incoming.IncomingPacketHandler
 import fyi.pauli.ichor.gaia.server.Server
-import io.ktor.network.sockets.*
 import java.nio.ByteBuffer
 
-internal suspend fun Connection.handleIncoming(handle: PacketHandle, server: Server) {
-	while (!input.isClosedForRead) {
+internal suspend fun PacketHandle.handleIncoming(server: Server) {
+	while (!connection.input.isClosedForRead) {
 		var size = 0
 		var i = 0
 		var b: Byte
 		do {
-			b = input.readByte()
+			b = connection.input.readByte()
 			size = size or ((b.toInt() and 0x7F) shl (i * 7))
 			i++
 		} while ((b.toInt() and 0x80) != 0 && i < 5)
 
 		val buffer = ByteBuffer.allocate(size)
-		input.readAvailable(buffer)
+		connection.input.readAvailable(buffer)
 		buffer.flip()
 
-		IncomingPacketHandler.deserializeAndHandle(buffer, handle, server)
+		IncomingPacketHandler.deserializeAndHandle(buffer, this, server)
 	}
 }
