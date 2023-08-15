557b710b1c98daab3c48dd375ba7d4658ac8d439
Anton <anton.wiblishauser@gmx.de>
2023-08-15 20:17:16 +0200

    add VAR LONG data type

--- gaia/src/main/kotlin/fyi/pauli/ichor/gaia/networking/DataTypes.kt
+++ gaia/src/main/kotlin/fyi/pauli/ichor/gaia/networking/DataTypes.kt
@@ -3,6 +3,7 @@ package fyi.pauli.ichor.gaia.networking
 const val VAR_INT = 5
 const val INT = 4
 const val LONG = 8
+const val VAR_LONG = 10
 const val SHORT = 2
 const val BOOLEAN = 1
 const val UUID = 16
