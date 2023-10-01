5ff3559e48a250c095088f9321f2da05a13f5a4b
btwonion <btwonion@gmx.de>
2023-10-01 15:58:32 +0200

    fix server startup

--- hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/Hephaistos.kt
+++ hephaistos/src/main/kotlin/fyi/pauli/ichor/hephaistos/Hephaistos.kt
@@ -1,29 +1,16 @@
 package fyi.pauli.ichor.hephaistos
 
 import fyi.pauli.ichor.gaia.server.Server
-import fyi.pauli.ichor.gaia.server.ichor
+import fyi.pauli.ichor.gaia.server.serve
 import fyi.pauli.ichor.hephaistos.networking.extensions.NetworkingExtensions
-import io.github.oshai.kotlinlogging.KLogger
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
-import org.koin.core.annotation.KoinExperimentalAPI
-import org.koin.core.context.startKoin
-import org.koin.dsl.lazyModule
 
-@OptIn(KoinExperimentalAPI::class)
-suspend fun main() = ichor(Hephaistos()) {
-	startKoin {
-		lazyModule {
-			single<Hephaistos> { this@ichor }
-			single<KLogger> { this@ichor.logger }
-		}
-	}
+suspend fun main() = serve(Hephaistos)
 
-}
-
-class Hephaistos : Server("Hephaistos") {
+object Hephaistos : Server("Hephaistos") {
 	override val httpClient: HttpClient = HttpClient(CIO) {
 		install(ContentNegotiation) {
 			json(Constants.json)
