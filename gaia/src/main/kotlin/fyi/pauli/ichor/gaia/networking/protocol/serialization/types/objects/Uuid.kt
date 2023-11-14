7ffa44a24bbd504a4af3447b2b692dc927a1c3ad
btwonion <btwonion@gmx.de>
2023-11-14 19:06:23 +0100

    code cleanup

--- gaia/src/main/kotlin/fyi/pauli/ichor/gaia/networking/protocol/serialization/types/objects/Uuid.kt
+++ gaia/src/main/kotlin/fyi/pauli/ichor/gaia/networking/protocol/serialization/types/objects/Uuid.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
-import java.util.UUID
+import java.util.*
 
 /**
  * @author btwonion
