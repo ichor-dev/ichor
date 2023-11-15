0e6bdb06bd06395e6fd7928b1f07b19e50a71dbf
btwonion <btwonion@gmx.de>
2023-11-15 18:13:16 +0100

    add Identifier serializer

--- gaia/src/main/kotlin/fyi/pauli/ichor/gaia/models/Identifier.kt
+++ gaia/src/main/kotlin/fyi/pauli/ichor/gaia/models/Identifier.kt
@@ -1,5 +1,9 @@
 package fyi.pauli.ichor.gaia.models
 
+import fyi.pauli.ichor.gaia.networking.protocol.serialization.types.objects.IdentifierByteSerializer
+import kotlinx.serialization.Serializable
+
+@Serializable(with = IdentifierByteSerializer::class)
 data class Identifier(val namespace: String, val value: String) {
 	override fun toString(): String {
 		return "$namespace:$value"
