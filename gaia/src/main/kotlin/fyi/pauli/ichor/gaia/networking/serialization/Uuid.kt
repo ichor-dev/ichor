9882b3ace23a0c352464bdccbe93d93ee81ee0ea
btwonion <btwonion@gmx.de>
2023-11-19 14:03:41 +0100

    add byteserializers

---
+++ gaia/src/main/kotlin/fyi/pauli/ichor/gaia/networking/serialization/Uuid.kt
@@ -0,0 +1,95 @@
+package fyi.pauli.ichor.gaia.networking.serialization
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+import java.util.*
+
+/**
+ * @author btwonion
+ * @since 14/11/2023
+ *
+ * Byte serializer for the java.util.UUID object.
+ */
+object UuidByteSerializer : KSerializer<UUID> {
+	override val descriptor: SerialDescriptor = buildClassSerialDescriptor("uuid") {
+		element<Long>("msb")
+		element<Long>("lsb")
+	}
+
+	override fun serialize(encoder: Encoder, value: UUID) {
+		encoder.encodeStructure(descriptor) {
+			encodeLongElement(descriptor, 0, value.mostSignificantBits)
+			encodeLongElement(descriptor, 1, value.leastSignificantBits)
+		}
+	}
+
+	@OptIn(ExperimentalSerializationApi::class)
+	override fun deserialize(decoder: Decoder): UUID {
+		return decoder.decodeStructure(descriptor) {
+			var mostSignificationBits: Long? = null
+			var leastSignificationBits: Long? = null
+
+			if (decodeSequentially()) {
+				mostSignificationBits = decodeLongElement(descriptor, 0)
+				leastSignificationBits = decodeLongElement(descriptor, 1)
+			} else {
+				while (true) {
+					when (val index = decodeElementIndex(descriptor)) {
+						0 -> mostSignificationBits = decodeLongElement(descriptor, 0)
+						1 -> leastSignificationBits = decodeLongElement(descriptor, 1)
+						CompositeDecoder.DECODE_DONE -> break
+						else -> error("Unexpected index: $index")
+					}
+				}
+			}
+
+			requireNotNull(mostSignificationBits)
+			requireNotNull(leastSignificationBits)
+
+			UUID(mostSignificationBits, leastSignificationBits)
+		}
+	}
+}
+
+/**
+ * String serializer for the java.util.UUID object.
+ */
+object UuidStringSerializer : KSerializer<UUID> {
+	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("uuid", PrimitiveKind.STRING)
+
+	override fun serialize(encoder: Encoder, value: UUID) {
+		encoder.encodeString(value.toString())
+	}
+
+	override fun deserialize(decoder: Decoder): UUID {
+		return UUID.fromString(decoder.decodeString())
+	}
+}
+
+/**
+ * String serializer for the old Minecraft java.util.UUID object.
+ */
+object OldUuidStringSerializer : KSerializer<UUID> {
+	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("old_uuid", PrimitiveKind.STRING)
+
+	override fun deserialize(decoder: Decoder): UUID {
+		val uuidWithoutHyphens = decoder.decodeString()
+		return UUID.fromString(
+			"${uuidWithoutHyphens.substring(0, 8)}-${
+				uuidWithoutHyphens.substring(
+					8, 12
+				)
+			}-${uuidWithoutHyphens.substring(12, 16)}-${
+				uuidWithoutHyphens.substring(
+					16, 20
+				)
+			}-${uuidWithoutHyphens.substring(20)}"
+		)
+	}
+
+	override fun serialize(encoder: Encoder, value: UUID) {
+		encoder.encodeString(value.toString().replace("-", ""))
+	}
+}
