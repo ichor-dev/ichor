cce1371ebdd92fae343121922d1cb648f23fb699
Paul Kindler <pa179kin@gmail.com>
2024-05-01 14:45:07 +0200

    moved some dependencies, update nbterialize & prorialize

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,8 @@ dependencyResolutionManagement {
     }
 
     create("ichor") {
-      library("prorialize", "fyi.pauli.prorialize", "prorialize").version("1.1.2")
-      library("nbterialize", "fyi.pauli.nbterialize", "nbterialize").version("1.0.2")
+      library("prorialize", "fyi.pauli.prorialize", "prorialize").version("1.1.3")
+      library("nbterialize", "fyi.pauli.nbterialize", "nbterialize").version("1.0.3")
 
       bundle("ichor", listOf("prorialize", "nbterialize"))
     }
@@ -46,33 +46,28 @@ dependencyResolutionManagement {
       )
     }
 
-    create("koin") {
+    create("crypto") {
+
+      library("core", "dev.whyoleg.cryptography", "cryptography-core").version("0.3.0")
+      library("providers-jdk", "dev.whyoleg.cryptography", "cryptography-provider-jdk").version("0.3.0")
+      library("providers-apple", "dev.whyoleg.cryptography", "cryptography-provider-apple").version("0.3.0")
+      library("providers-openssl", "dev.whyoleg.cryptography", "cryptography-provider-openssl3-api").version("0.3.0")
+
+    }
+
+    create("libs") {
+      library("uuid", "com.benasher44", "uuid").version("0.8.4")
+
+      library("bignum", "com.ionspin.kotlin", "bignum").version("0.3.9")
+
+      library("logging", "io.github.oshai", "kotlin-logging").version("6.0.9")
+
+      library("ktoml", "com.akuleshov7", "ktoml-core").version("0.5.1")
+
       library("core", "io.insert-koin", "koin-core").version("3.5.6")
       library("coroutines", "io.insert-koin", "koin-core-coroutines").version("3.5.6")
 
       bundle("koin", listOf("core", "coroutines"))
-    }
-
-    create("klogging") {
-      library("logging", "io.github.oshai", "kotlin-logging").version("6.0.9")
-
-      bundle("logging", listOf("logging"))
-    }
-
-    create("fileConfiguration") {
-      version("ktoml", "0.5.1")
-
-      library("ktoml-core", "com.akuleshov7", "ktoml-core").versionRef("ktoml")
-
-      bundle("toml", listOf("ktoml-core"))
-    }
-
-    create("cryptography"){
-      library("core", "dev.whyoleg.cryptography", "cryptography-core").version("0.3.0")
-    }
-
-    create("kn") {
-      library("uuid", "com.benasher44", "uuid").version("0.8.4")
     }
 
     create("testing") {
