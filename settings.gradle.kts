84ef253d131d913679d8b8a40aa9b0bc355caa38
Paul Kindler <pa179kin@gmail.com>
2024-05-28 17:10:07 +0200

    update version catalog, add .kotlin to .gitignore -> kotlin 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
   }
   versionCatalogs {
     create("ichor") {
-      from("fyi.pauli:ichor-catalog:1.6.1")
+      from("fyi.pauli:ichor-catalog:1.6.3")
     }
   }
 }
